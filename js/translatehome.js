ac6ff1ecbe67d55d6f491a587ea97f1b4e710e33
xblxgxd <octopus10032005@gmail.com>
2024-11-13 15:57:45 +0300

    создание переводов для главной страницы

---
+++ js/translatehome.js
@@ -0,0 +1,52 @@
+var translations = {};
+
+function loadTranslations() {
+  return fetch('/js/translationhome.json')
+    .then(response => response.json())
+    .then(data => {
+      translations = data;
+    })
+    .catch(error => console.error('Error loading translations:', error));
+}
+
+function toggleLanguage() {
+  var currentLanguage = document.documentElement.lang || 'en';
+  localStorage.setItem('language','en');
+  if (currentLanguage === 'en') {
+    applyTranslations('ru');
+    document.documentElement.lang = 'ru';
+    document.getElementById('translateBtn').textContent = 'Русский';
+    localStorage.setItem('language','ru');
+  } else {
+    applyTranslations('en');
+    document.documentElement.lang = 'en';
+    document.getElementById('translateBtn').textContent = 'English';
+    localStorage.setItem('language','en');
+  }
+}
+
+function applyTranslations(lang) {
+  var translation = translations[lang];
+
+  var elements = document.querySelectorAll('[id]');
+  elements.forEach(function(element) {
+    var translationKey = element.getAttribute('id');
+    if (translationKey && translation[translationKey]) {
+      element.innerHTML = translation[translationKey];
+    }
+  });
+}
+
+loadTranslations().then(() => {
+  var userLanguage = navigator.language.substr(0, 2); 
+  if (localStorage.getItem('language')==='en') {
+    applyTranslations('en');
+    document.documentElement.lang = 'en';
+    document.getElementById('translateBtn').textContent = 'English';
+    
+  } else {
+    applyTranslations('ru');
+    document.documentElement.lang = 'ru';
+    document.getElementById('translateBtn').textContent = 'Русский';
+  }
+});
